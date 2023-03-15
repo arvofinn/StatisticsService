7a44e11b71d85ec898999dae660d53add0ac9b9f
Arvo <arvofinland@gmail.com>
2023-03-15 22:50:27 +0300

    add new coverage test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void findAbsoluteMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 40, 8, 6, 11, 11, 12};
+        long expected = 40;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
